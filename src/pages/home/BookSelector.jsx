3c4339152eb0d81a532c309cb1646b1b51383354
Sam S <salinas144@gmail.com>
2019-07-05 15:00:39 -0400

    Fix race condition in BookSelector blur event Reason: Worked fine on my desktop, but not on my laptop.. How this change addresses the issue: Stops using setTimeout and now uses event.relatedTarget in the blur event to prevent unfocusing the search input before the a book click event. Side Effects: Does not work on <= IE 11.

--- src/pages/home/BookSelector.jsx
+++ src/pages/home/BookSelector.jsx
@@ -1,101 +1,112 @@
-import React from 'react';
-import {
-    getBookNames
-} from '../../helpers/bibleHelper';
+import React from "react";
+import { getBookNames } from "../../helpers/bibleHelper";
 import { StateContext } from "./../../State";
 import { selectBook } from "../../modules/homeIndexData";
 
-import './BookSelector.css';
+import "./BookSelector.css";
 
 export default class BookSelector extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            bookNames: getBookNames(),
-            searchInput: 'Genesis',
-            searchInputFocus: false,
-            persistentBookName: 'Genesis'
-        }
+  constructor() {
+    super();
+
+    this.results = React.createRef();
+
+    this.state = {
+      bookNames: getBookNames(),
+      searchInput: "Genesis",
+      searchInputFocus: false,
+      persistentBookName: "Genesis"
+    };
+  }
+
+  onBookChange = event => {
+    this.setState({
+      searchInput: event.target.value
+    });
+  };
+
+  onBookSelect = book => {
+    this.setState({
+      searchInput: book.name,
+      searchInputFocus: false,
+      persistentBookName: book.name
+    });
+    // const [ { homeIndexData }, dispatch ] = this.context;
+    const dispatch = this.context[1];
+    dispatch(selectBook(book.index));
+  };
+
+  toggleSearchInputFocus = (event, val) => {
+    if (val) {
+      this.setState({
+        searchInput: "",
+        searchInputFocus: true
+      });
+    } else {
+      if (
+        event.relatedTarget &&
+        event.relatedTarget.parentElement &&
+        event.relatedTarget.parentElement === this.results.current
+      ) {
+        return;
+      }
+
+      this.setState({
+        searchInputFocus: false,
+        searchInput: this.state.persistentBookName
+      });
     }
+  };
 
-    onBookChange = (event) => {
-        this.setState({
-            searchInput: event.target.value
-        })
-    }
+  bookResultsHtml = () => {
+    const { searchInput, bookNames, searchInputFocus } = this.state;
 
-    onBookSelect = (book) => {
-        clearTimeout(this.timeoutid);
-        this.setState({
-            searchInput: book.name,
-            searchInputFocus: false,
-            persistentBookName: book.name,
-        })
-        // const [ { homeIndexData }, dispatch ] = this.context;
-        const dispatch = this.context[1];
-        dispatch(selectBook(book.index));
-    }
-    
-    toggleSearchInputFocus = (val, timeout) => {
-        if (timeout === undefined) {
-            this.setState({
-                searchInputFocus: val
-            })
-        } else {
-            this.timeoutid = setTimeout(() => {
-                this.setState({
-                    searchInputFocus: val,
-                    searchInput: this.state.persistentBookName
-                })
-            }, timeout)
-        }
-    }
+    if (!searchInputFocus) return null;
 
-    bookResultsHtml = () => {
-        const { searchInput, bookNames, searchInputFocus } = this.state;
+    let res = bookNames.filter(book => {
+      return book.name.toLowerCase().includes(searchInput.toLowerCase());
+    });
 
-        if (!searchInputFocus) return null;
-
-        let res = bookNames.filter(book => {
-            return book.name.toLowerCase().includes(searchInput.toLowerCase());
-        })
-
-        return (
-            <div className="home-book-results">
-                {res.map((book, key) => {
-                    return (
-                    <div
-                        key={key}
-                        className="home-book-single-result"
-                        onClick={() => this.onBookSelect(book)}
-                    >
-                    {book.name}
-                    </div>)
-                })}
+    return (
+      <div
+        className="home-book-results"
+        id="home-book-results"
+        ref={this.results}
+      >
+        {res.map((book, key) => {
+          return (
+            <div
+              key={key}
+              className="home-book-single-result"
+              onClick={() => this.onBookSelect(book)}
+              tabIndex={0}
+            >
+              {book.name}
             </div>
-        )
-    }
-    
-    render = () => {
-        const { searchInput } = this.state;
+          );
+        })}
+      </div>
+    );
+  };
 
-        return (
-            <div className="home-search-container">
-                <input
-                    className="home-book-search-input"
-                    type='text' value={searchInput}
-                    onChange={this.onBookChange}
-                    onFocus={() => {
-                        this.toggleSearchInputFocus(true)
-                        this.setState({
-                            searchInput: ''
-                        })
-                    }}
-                    onBlur={() => this.toggleSearchInputFocus(false, 200)}
-                />
-                {this.bookResultsHtml()}
-            </div>
-        )
-    }
+  render = () => {
+    const { searchInput } = this.state;
+
+    return (
+      <div className="home-search-container">
+        <input
+          className="home-book-search-input"
+          type="text"
+          value={searchInput}
+          onChange={this.onBookChange}
+          onFocus={event => {
+            this.toggleSearchInputFocus(event, true);
+          }}
+          onBlur={event => this.toggleSearchInputFocus(event, false)}
+        />
+        {this.bookResultsHtml()}
+      </div>
+    );
+  };
 }
 BookSelector.contextType = StateContext;
