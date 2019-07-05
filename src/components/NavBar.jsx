d3aeb26f2717409f872db4b3dfc06baa8f17966a
LfxB <13532850+LfxB@users.noreply.github.com>
2019-07-05 02:52:28 -0400

    Update Navigation bar to be responsive Reason: Should work on any screen size now.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,48 @@ import { Link } from 'react-router-dom'
 import './NavBar.css'
 
 export default class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            navbarClassName: "navbar-ul"
+        }
+    }
+
+    toggleResponsiveMenu = () => {
+        console.log("yo");
+        const { navbarClassName } = this.state;
+        if (navbarClassName === "navbar-ul") {
+            this.setState({
+                navbarClassName: "navbar-ul responsive"
+            })
+        } else {
+            this.setState({
+                navbarClassName: "navbar-ul"
+            })
+        }
+    }
+
     render = () => {
+        const { navbarClassName } = this.state;
         return (
-            <ul className="navbar-ul">
+            <ul className={navbarClassName}>
                 <li>
                     <Link className="navbar-active" to="/">
+                        <i className="material-icons navbar-icon">book</i>
                         Bible Verse Display
                     </Link>
                 </li>
-                <li><a href="https://codepen.io/LfxB/full/LQebBx">Old Version</a></li>
+                <li>
+                    <a href="https://codepen.io/LfxB/full/LQebBx">
+                        <i className="material-icons navbar-icon">swap_horiz</i>
+                        Old Version
+                    </a>
+                </li>
+                <li className="navbar-menu-button">
+                    <button onClick={this.toggleResponsiveMenu}>
+                        <i className="material-icons navbar-icon">menu</i>
+                    </button>
+                </li>
             </ul>
         )
     }
