8aa10402dd573129c2392ad2c571a1b9cf29c2d2
LfxB <13532850+LfxB@users.noreply.github.com>
2019-07-20 18:09:03 -0400

    Add Popup Status under navbar Reason: Added so the presenter doesn't need to look up at the second monitor to make sure the verse changed.
    To do:
    Give it an animation when it changes so that it is more noticeable.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import PopupStatus from './PopupStatus'
+
 import './NavBar.css'
 
 export default class NavBar extends React.Component {
@@ -27,25 +29,28 @@ export default class NavBar extends React.Component {
     render = () => {
         const { navbarClassName } = this.state;
         return (
-            <ul className={navbarClassName}>
-                <li>
-                    <Link className="navbar-active" to="/">
-                        <i className="material-icons navbar-icon">book</i>
-                        Bible Verse Display
-                    </Link>
-                </li>
-                <li>
-                    <a href="https://codepen.io/LfxB/full/LQebBx">
-                        <i className="material-icons navbar-icon">swap_horiz</i>
-                        Old Version
-                    </a>
-                </li>
-                <li className="navbar-menu-button">
-                    <button onClick={this.toggleResponsiveMenu}>
-                        <i className="material-icons navbar-icon">menu</i>
-                    </button>
-                </li>
-            </ul>
+            <React.Fragment>
+                <ul className={navbarClassName}>
+                    <li>
+                        <Link className="navbar-active" to="/">
+                            <i className="material-icons navbar-icon">book</i>
+                            Bible Verse Display
+                        </Link>
+                    </li>
+                    <li>
+                        <a href="https://codepen.io/LfxB/full/LQebBx">
+                            <i className="material-icons navbar-icon">swap_horiz</i>
+                            Old Version
+                        </a>
+                    </li>
+                    <li className="navbar-menu-button">
+                        <button onClick={this.toggleResponsiveMenu}>
+                            <i className="material-icons navbar-icon">menu</i>
+                        </button>
+                    </li>
+                </ul>
+            <PopupStatus />
+            </React.Fragment>
         )
     }
 }
---
+++ src/components/PopupStatus.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StateContext } from "../State"
+import { hideVerseWindow } from "../modules/verseWindowData"
+import { getBookFromIndex } from "../helpers/bibleHelper"
+
+import "./PopupStatus.css"
+
+export default class PopupStatus extends React.Component {
+    render = () => {
+        const [ { verseWindowData } ] = this.context;
+
+        const { showVerseWindow, bookIndex, chapterIndex, verseIndex } = verseWindowData;
+
+        if (!showVerseWindow) return null;
+
+        return (
+            <div className="popup-status">
+                <span>Currently viewing:</span>
+                {
+                    " " +
+                    getBookFromIndex(bookIndex) + " "
+                    + (chapterIndex + 1).toString() + ":"
+                    + (verseIndex + 1).toString()
+                }
+            </div>
+        )
+    }
+}
+PopupStatus.contextType = StateContext;
